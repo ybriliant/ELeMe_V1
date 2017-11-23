fb423c64d0f493760de6fb674ee2f2e93427b656
Tony <tony@gmail.com>
2017-11-23 08:51:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ggb/eleme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ggb.eleme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
