62f25cbd3d321fa5a435908af7f24337c6236bcc
ybriliant <912828340@qq.com>
2017-12-01 11:44:19 +0800

    right

--- app/src/main/java/com/example/ggb/eleme/xd/LunBo.java
+++ app/src/main/java/com/example/ggb/eleme/xd/LunBo.java
@@ -25,7 +25,7 @@ public class LunBo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lunbo);
+        setContentView(R.layout.activity_take_out);
 
         mRollViewPager = (RollPagerView) findViewById(R.id.roll_view_paper);
 
