2c6291bb1112b9b611974ec0c2b116ab70ecc584
ybriliant <912828340@qq.com>
2017-12-11 14:46:20 +0800

    anew

---
+++ app/src/main/java/com/a1141705068qq/main/imp/ShopCartImp.java
@@ -0,0 +1,11 @@
+package com.a1141705068qq.main.imp;
+
+import android.view.View;
+
+/**
+ * Created by cheng on 16-11-13.
+ */
+public interface ShopCartImp {
+    void add(View view, int postion);
+    void remove(View view, int postion);
+}
