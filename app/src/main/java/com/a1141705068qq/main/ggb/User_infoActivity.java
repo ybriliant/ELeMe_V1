69258b026ab018654a77649afdacf211852eb7f8
GGB <ggb@10086.com>
2017-12-20 17:50:11 +0800

    商品网络加载完善

---
+++ app/src/main/java/com/a1141705068qq/main/ggb/User_infoActivity.java
@@ -0,0 +1,16 @@
+package com.a1141705068qq.main.ggb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.a1141705068qq.class_one.R;
+
+public class User_infoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_info);
+    }
+}
