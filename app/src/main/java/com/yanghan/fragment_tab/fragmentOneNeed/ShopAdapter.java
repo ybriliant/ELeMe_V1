13bac24f633f6cb880db47a20eea2e12112689f9
governormars <466375758@qq.com>
2017-12-25 21:34:01 +0800

    Merge branch 'master' of C:\Users\46637\AndroidStudioProjects\ELeMe_V1 with conflicts.

--- app/src/main/java/com/yanghan/fragment_tab/fragmentOneNeed/ShopAdapter.java
+++ app/src/main/java/com/yanghan/fragment_tab/fragmentOneNeed/ShopAdapter.java
@@ -11,8 +11,6 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.a1141705068qq.class_one.R;
-import com.a1141705068qq.main.MainActivity;
-import com.bumptech.glide.Glide;
 
 import java.util.List;
 import java.util.Map;
@@ -87,11 +85,10 @@ public class ShopAdapter extends BaseAdapter {
             shop=(Shop)convertView.getTag();
         }
         //绑定数据
-       // shop.image_restaurant.setBackgroundResource((Integer)data.get(position).get("image_restaurant"));
-        Glide.with(context).load(data.get(position).get("image_restaurant")).into(shop.image_restaurant);
+        shop.image_restaurant.setBackgroundResource((Integer)data.get(position).get("image_restaurant"));
         shop.name_restaurant.setText((String)data.get(position).get("name_restaurant"));
-        shop.ratingbar1.setNumStars((Integer)data.get(position).get("ratingbar1"));
-        shop.ratingbar1.setRating((Float)data.get(position).get("ratingbar2"));
+        shop.ratingbar1.setNumStars((Integer) data.get(position).get("ratingbar1"));
+        shop.ratingbar1.setRating((Float)data.get(position).get("fuck"));
         shop.start_send.setText((String)data.get(position).get("start_send"));
         shop.send_fee.setText((String)data.get(position).get("send_fee"));
         shop.arrive_time_restaurant.setText((String)data.get(position).get("arrive_time_restaurant"));
